96dc59b333bb99ce1a6d6ee0df05b4b6ed8dfa18
Murali07 <muraliselvaraj97@gmail.com>
2022-10-19 17:39:23 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./components/admin/Dashboard";
+import Login from "./components/users/Login";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/users/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../styles/Login.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from 'react-bootstrap/Spinner';
+import env from "../../env";
+
+function Login() {
+
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [toggle, setToggle] = useState("");
+    let [message, setMessage] = useState("");
+    let navigate = useNavigate();
+
+    let handleLogin = async () => {
+        setToggle(true)
+        let res = await axios.post(`${env.apiurl}/users/login`, {
+            email,
+            password
+        })
+        if(res.data.stausCode === 200){
+            setToggle(false)
+            sessionStorage.setItem("token", res.data.token)
+            sessionStorage.setItem("role", res.data.role)
+            sessionStorage.setItem("userId", res.data.userId)
+            if(res.data.role === "admin"){
+                navigate("/dashboard")
+            }else{
+                navigate("/user-page")
+            }
+        }else{
+            setToggle(false)
+            setMessage(res.data.message)
+            setTimeout(()=>{
+                setMessage("")
+                setEmail("")
+                setPassword("")
+            },3000)
+        }
+    }
+
+
+  return (
+    <div className="loginpage">
+      <div className="login-wrapper">
+        <h1>Welcome to our App</h1>
+        <p>Login to Continue</p>
+      </div>
+      <div className="login-main-wrapper">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" onClick={() => handleLogin()}>
+            Submit
+          </Button>
+        </Form>
+        {toggle ? <Spinner animation="border" variant="primary" /> : <></>}
+        {message ? <div style={{"color" : "red", "textAlign" : "center"}} > {message} </div> : <></>}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
